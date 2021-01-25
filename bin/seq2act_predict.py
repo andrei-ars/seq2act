9fb3d7f4ebde91444441881287e6863b10f870b7
Andrei <z@sp.sp>
2021-01-25 18:21:41 +0300

    1

--- bin/seq2act_predict.py
+++ bin/seq2act_predict.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 from tensor2tensor.utils import trainer_lib
 import tensorflow.compat.v1 as tf
 
